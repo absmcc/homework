76bd8521404b9f1f584559bc44a8931c749f2281
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-14 10:40:23 -0400

    created homework folder

---
+++ daythree.js
@@ -0,0 +1,31 @@
+
+
+// let car = {
+// model: "Lexus",
+// color: "red",
+
+// }
+// car.year = 2022
+// console.log(car.model)
+// console.log(car.color)
+// console.log(car.year)
+
+
+// let fruit = ["apples", "avocados", "oranges"] 
+// console.log(fruit[0])
+
+
+// function firstElement(arr){
+//     console.log(arr[0])
+// }
+
+// let weekdays = ["Monday", "Tuesday", "Wednesday"]
+// firstElement(weekdays)
+
+// function sumOfNums(x,y){
+// return x + y
+
+// }
+//  num1 = 10 
+//  num2 = 25 
+// console.log(sumOfNums(num1 + num2))
